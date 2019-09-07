ebfdcc7fd201c63d3fc5e5f1ae1d5457f93d300b
murad1437 <feb4our@gmail.com>
2019-09-07 13:41:53 -0700

    Fixed

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Murad shahin bitches!</h1>
+        <h1>Murad shahin bitches! we love u so much</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
